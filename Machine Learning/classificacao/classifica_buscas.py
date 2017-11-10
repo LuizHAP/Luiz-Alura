7265ba3a69e54e5be9bb5759c7b427e6d5ab37aa
Luiz Henrique <lpansarini@gmail.com>
2017-11-09 22:26:51 -0200

    09/11 - Após explicação do Forum Alura

--- Machine Learning/classificacao/classifica_buscas.py
+++ Machine Learning/classificacao/classifica_buscas.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 from collections import Counter
 
@@ -21,30 +22,30 @@ tamanho_de_validacao =  len(Y) - tamanho_de_treino - tamanho_de_teste
 treino_dados = X[0:tamanho_de_treino]
 treino_marcacoes = Y[0:tamanho_de_treino]
 
-fim_de_treino = int(tamanho_de_treino+ tamanho_de_teste)
-teste_dados = X[tamanho_de_treino:fim_de_treino]
-teste_marcacoes = Y[tamanho_de_treino:fim_de_treino]
+fim_de_teste = int(tamanho_de_treino+ tamanho_de_teste)
+teste_dados = X[tamanho_de_treino:fim_de_teste]
+teste_marcacoes = Y[tamanho_de_treino:fim_de_teste]
 
-fim_de_teste = int(fim_de_treino+ tamanho_de_treino)
+
 validacao_dados = X[fim_de_teste:]
 validacao_marcacoes = Y[fim_de_teste:]
 
 def fit_and_predict(nome, modelo, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes):
-	modelo.fit(treino_dados, treino_marcacoes)
+    modelo.fit(treino_dados, treino_marcacoes)
 
-	resultado = modelo.predict(teste_dados)
+    resultado = modelo.predict(teste_dados)
 
-	acertos = resultado == teste_marcacoes
+    acertos = resultado == teste_marcacoes
 
-	total_de_acertos = sum(acertos)
-	total_de_elementos = len(teste_dados)
+    total_de_acertos = sum(acertos)
+    total_de_elementos = len(teste_dados)
 
-	taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+    taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
 
-	msg = "Taxa de acerto do {0}: {1}".format(nome, taxa_de_acerto)
-	print(msg)
+    msg = "Taxa de acerto do {0}: {1}".format(nome, taxa_de_acerto)
+    print(msg)
 
-	return taxa_de_acerto
+    return taxa_de_acerto
 
 from sklearn.naive_bayes import MultinomialNB
 modeloMultinomial = MultinomialNB()
@@ -55,13 +56,13 @@ modeloAdaBoost = AdaBoostClassifier()
 resultadoAdaBoost = fit_and_predict("AdaBoostClassifier", modeloAdaBoost, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes)
 
 if (resultadoMultinomialNB > resultadoAdaBoost):
-	vencedor = resultadoMultinomialNB
-else:
-	vencedor = resultadoAdaBoost
+    vencedor = modeloMultinomialNB
+else:    
+    vencedor = modeloAdaBoost
 
 resultado = vencedor.predict(validacao_dados)
 
-acertos = resultado - validacao_marcacoes
+acertos = (resultado == validacao_marcacoes)
 
 total_de_acertos = sum(acertos)
 total_de_elementos = len(validacao_marcacoes)
@@ -71,8 +72,7 @@ taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
 msg = "Taxa de acerto do vencedor entre os dois algoritmos no mundo real {0}".format(taxa_de_acerto)
 print(msg)
 
-acerto_base = max(Counter(teste_marcacoes).itervalues())
-taxa_de_acerto_base = 100.0 * acerto_base / len(teste_marcacoes)
+acerto_base = max(Counter(validacao_marcacoes).itervalues())
+taxa_de_acerto_base = 100.0 * acerto_base / len(validacao_marcacoes)
 print("Taxa de acerto base: %f" % taxa_de_acerto_base)
-
-print("Total de testes: %d" %len(teste_dados))
+print("Total de testes: %d" %len(validacao_dados))
